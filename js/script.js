7ec86a6e3429ab1b6d81e177e451803510e341f9
EliBrank <eliasbrinkman@gmail.com>
2024-12-04 14:36:11 -0600

    get multi-item carousel working with api data

--- js/script.js
+++ js/script.js
@@ -1,63 +1,40 @@
 function buildMultiCarouselItem(item) {
-  const $slide = $('div').addClass('swiper-slide d-flex justify-content-center');
-  const $card = $('div').addClass('card');
-  const $thumbnail = $('img').attr({
-    'src': item.thumb_url,
-    'alt': 'Video thumbnail'
-  }).addClass('card-img-top');
-  const $playButtonContainer = $('div').addClass('card-img-overlay text-center');
-  const $playButton = $('img').attr({
-    'src': 'images/play.png',
-    'alt': 'Play',
-    'width': '64px'
-  }).addClass('align-self-center play-overlay');
+  const $slide = $('<div>').addClass('swiper-slide d-flex justify-content-center');
+  $slide.html(`
+    <div class="card">
+      <img src=${item.thumb_url} class="card-img-top" alt="Video thumbnail" />
+      <div class="card-img-overlay text-center">
+        <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay" />
+      </div>
+      <div class="card-body">
+        <h5 class="card-title font-weight-bold">${item.title}</h5>
+        <p class="card-text text-muted">${item['sub-title']}</p>
+        <div class="creator d-flex align-items-center">
+          <img src=${item.author_pic_url} alt="Creator of Video" width="30px" class="rounded-circle" />
+          <h6 class="pl-3 m-0 main-color">${item.author}</h6>
+        </div>
+        <div class="info pt-3 d-flex justify-content-between">
+          <div class="rating">
+          </div>
+          <span class="main-color">${item.duration}</span>
+        </div>
+      </div>
+    </div>
+  `);
 
-  $playButtonContainer.append($playButton);
+  let $videoRating = $slide.find('.rating')
 
-  // card body
-  const $cardBody = $('div').addClass('card-body');
-  const $cardTitle = $('h5').addClass('card-title font-weight-bold').text('Diagonal Smile');
-  const $cardText = $('p').addClass('card-text text-muted')
-    .text('Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod.');
-  const $videoAuthorContainer = $('div').addClass('creator d-flex align-self-center');
-  const $videoAuthorPhoto = $('img').attr({
-    'src': item.pic_url,
-    'alt': 'Creator of Video',
-    'width': '30px',
-  }).addClass('rounded-circle');
-  const $videoAuthorName = $('h6').addClass('pl-3 m-0 main-color').text('Phillip Massey');
-  
-  $videoAuthorContainer.append($videoAuthorPhoto, $videoAuthorName);
-  
-  const $videoInfo = $('div').addClass('info pt-3 d-flex justify-content-between');
-  const $videoRating = $('div').addClass('rating');
-
-  // add stars to rating
-  for (let i = 0; i < 5; i++) {
-    const $starOn = $('img').attr({
-      'src': 'images/star_on.png',
-      'alt': 'star on',
+  // create stars
+  // initialize empty object of length 5, loop through values
+  const stars = Array.from({ length: 5 }, (_, i) => {
+    return $('<img>').attr({
+      'src': i < item.star ? 'images/star_on.png' : 'images/star_off.png',
+      'alt': i < item.star ? 'star on' : 'star off',
       'width': '15px'
     });
-    const $starOff = $('img').attr({
-      'src': 'images/star_off.png',
-      'alt': 'star off',
-      'width': '15px'
-    });
-    if (i < item.star) {
-      $videoRating.append($starOn);
-    } else {
-      $videoRating.append($starOff);
-    }
-  }
+  });
 
-  const $videoLength = $('span').addClass('main-color').text(item.duration);
-
-  // put elements together
-  $videoInfo.append($videoRating, $videoLength);
-  $cardBody.append($cardTitle, $cardText, $videoAuthorContainer, $videoInfo);
-  $card.append($thumbnail, $playButtonContainer, $cardBody);
-  $slide.append($card);
+  $videoRating.append(stars);
 
   return ($slide);
 }
@@ -70,7 +47,7 @@ function buildMultiItemCarousel(section) {
     method: 'GET',
     // api returns array of objects, iterate through
     success: function(response) {
-      const carouselContainer = document.querySelector(`#carousel-${section} .swiper-wrapper`);
+      const carouselContainer = $(`#carousel-${section} .swiper-wrapper`);
       response.forEach(item => {
         carouselContainer.append(buildMultiCarouselItem(item));
       });
